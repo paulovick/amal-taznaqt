d0df13163a394f48c8aad90c149aa38ce7483b7f
Pau Torrents <pau.torrents@thoughtworks.com>
2019-10-12 22:15:31 +0200

    Added return flights

--- src/pages/FlightsPage/Flight/Flight.js
+++ src/pages/FlightsPage/Flight/Flight.js
@@ -6,7 +6,7 @@ import { FlightAirport } from "../FlightAirport/FlightAirport"
 import Moment from "react-moment"
 import { AirlineLogo } from "../AirlineLogo/AirlineLogo"
 
-const Flight = ({
+const SingleFlightDetail = ({
   origin,
   destination,
   minimumPrice,
@@ -14,36 +14,50 @@ const Flight = ({
   carrier
 }) => {
   return (
+    <div className="flight">
+      <div className="flight-details">
+        <div>
+          <FlightAirport {...origin} />
+          <div className="flight-route-separator">-</div>
+          <FlightAirport {...destination} />
+        </div>
+        <div className="flight-date-time">
+          {departureDate &&
+          <>
+            <span>
+              <Moment format="DD MMM YYYY" locale="es">{departureDate}</Moment>
+            </span>
+            <span>
+              <Moment format="hh:mm" locale="es">{departureDate}</Moment>
+            </span>
+          </>
+          }
+        </div>
+      </div>
+      <div className="flight-carriers">
+        <AirlineLogo airline={carrier.Name} />
+      </div>
+      <div className="flight-price">
+        <div>
+          {minimumPrice.toFixed(2)} €
+        </div>
+        <div className="flight-min-price-label">Precio mínimo</div>
+      </div>
+    </div>
+  )
+}
+
+const Flight = ({ outboundFlight, returnFlight }) => {
+  return (
     <Card>
-      <CardBody className="flight">
-        <div className="flight-details">
-          <div>
-            <FlightAirport {...origin} />
-            <div className="flight-route-separator">-</div>
-            <FlightAirport {...destination} />
-          </div>
-          <div className="flight-date-time">
-            {departureDate &&
-              <>
-                <span>
-                  <Moment format="DD MMM YYYY" locale="es">{departureDate}</Moment>
-                </span>
-                <span>
-                  <Moment format="hh:mm" locale="es">{departureDate}</Moment>
-                </span>
-              </>
-            }
-          </div>
-        </div>
-        <div className="flight-carriers">
-          <AirlineLogo airline={carrier.Name} />
-        </div>
-        <div className="flight-price">
-          <div>
-            {minimumPrice.toFixed(2)} €
-          </div>
-          <div className="flight-min-price-label">Precio mínimo</div>
-        </div>
+      <CardBody>
+        <SingleFlightDetail {...outboundFlight} />
+        {returnFlight &&
+          <>
+            <div className="flights-separator" />
+            <SingleFlightDetail {...returnFlight} />
+          </>
+        }
       </CardBody>
     </Card>
   )
--- src/pages/FlightsPage/FlightsPage.js
+++ src/pages/FlightsPage/FlightsPage.js
@@ -5,15 +5,32 @@ import { Col, Container, Row } from "reactstrap"
 
 class FlightsPage extends Component {
   state = {
-    flights: null
+    outboundFlights: [],
+    returnFlights: []
   }
 
-  async componentDidMount() {
-    const flights = await fetchFlights('BCN-sky', 'MA-sky', '2019-12')
-    this.setState({ flights })
+  componentDidMount() {
+    fetchFlights('BCN-sky', 'MA-sky', '2019-11')
+      .then(outboundFlights => {
+        this.setState({ outboundFlights })
+      })
+    fetchFlights('MA-sky', 'BCN-sky', '2019-11')
+      .then(returnFlights => {
+        this.setState({ returnFlights })
+      })
+  }
+
+  getCheapestFlight = (flights) => {
+    return flights.sort(
+      (x, y) => x.minimumPrice < y.minimumPrice ? -1 :
+        x.minimumPrice === y.minimumPrice ? 0 : 1
+    )[0]
   }
 
   render() {
+    const cheapestOutboundFlight = this.getCheapestFlight(this.state.outboundFlights)
+    const cheapestReturnFlight = this.getCheapestFlight(this.state.returnFlights)
+
     return (
       <Container>
         <Row>
@@ -21,9 +38,17 @@ class FlightsPage extends Component {
             <h2 className="title">Vuelos</h2>
           </Col>
         </Row>
-        {this.state.flights && this.state.flights.map(flight =>
-          <Flight {...flight} />
-        )}
+
+        <h3>Ida y vuelta más baratos</h3>
+        {cheapestOutboundFlight && !cheapestReturnFlight &&
+          <Flight outboundFlight={cheapestOutboundFlight} />
+        }
+        {cheapestOutboundFlight && cheapestReturnFlight &&
+          <Flight outboundFlight={cheapestOutboundFlight} returnFlight={cheapestReturnFlight}/>
+        }
+        {!cheapestOutboundFlight && cheapestReturnFlight &&
+          <Flight outboundFlight={cheapestReturnFlight} />
+        }
       </Container>
     )
   }
