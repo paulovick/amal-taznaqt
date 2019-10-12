c2b99115233daa8fdd24d727b1389486bc008387
Marc Capdevila <cramca49@gmail.com>
2019-10-12 23:02:16 +0200

    About - done

--- src/pages/LandingPage/components/AboutUs/AboutUs.js
+++ src/pages/LandingPage/components/AboutUs/AboutUs.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Col, Container, Row } from "reactstrap"
+import './AboutUs.scss'
 
 const AboutUs = () => {
   return (
@@ -21,22 +22,22 @@ const AboutUs = () => {
                 className="image-container image-left"
                 style={{
                   backgroundImage:
-                    "url(" + require("assets/img/landing-detail-1.jpg") + ")"
+                    "url(" + require("assets/img/landing-detail-1.jpg") + ")",
+                    borderColor: '#437336'
                 }}
               >
-                <p className="blockquote blockquote-info">
-                  "Over the span of the satellite record, Arctic sea ice has
-                  been declining significantly, while sea ice in the
-                  Antarctichas increased very slightly" <br></br>
+                <p style={{color:'#437336'}} className="blockquote blockquote-info comment-container">
+                  "Ha sido una experiencia increíble, era mi primer voluntariado y estoy segura que no sera el último. He disfrutado con los niños y las niñas, he conocido diferentes maneras de vivir, he visitado la zona y los alrededores, una auténtica pasada. Emocionalmente me ha llenado al completo." <br></br>
                   <br></br>
-                  <small>-NOAA</small>
+                  <small style={{color:'#437336'}}>-Voluntario anonimo</small>
                 </p>
               </div>
               <div
                 className="image-container"
                 style={{
                   backgroundImage:
-                    "url(" + require("assets/img/landing-detail-3.jpg") + ")"
+                    "url(" + require("assets/img/landing-detail-3.jpg") + ")",
+                    marginTop: '700px',
                 }}
               />
             </Col>
@@ -49,18 +50,27 @@ const AboutUs = () => {
                 }}
               />
               <h3>
-                So what does the new record for the lowest level of winter
-                ice actually mean
+                Los principales objetivos de la asociación son:
               </h3>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eros dolor, blandit quis aliquam vitae, commodo ac massa. Quisque scelerisque consequat tempus. Nunc non justo dignissim, fermentum elit ut, vestibulum ipsum. Etiam et commodo nisi, vitae rutrum nunc. Proin facilisis ligula eu nunc sodales, et feugiat ante cursus. Aliquam consectetur ante non elit consequat, ac dictum tellus consequat. Sed vitae nulla laoreet, tempus arcu id, elementum metus. Donec ac efficitur sapien. Praesent arcu dolor, efficitur quis sapien vitae, dapibus rhoncus felis. Nam ut lacus vitae ante consectetur viverra. Proin eget euismod metus. Vestibulum laoreet, sem at vestibulum fermentum, sem lorem vehicula ex, eget commodo ipsum risus maximus arcu. Ut convallis suscipit turpis sit amet laoreet. Cras orci leo, gravida ut accumsan a, aliquam eget felis.
+                –  Atender la pobreza y la falta de recursos de la población de la zona, en la medida que nos sea posible,  proporcionando bienes materiales básicos para los habitantes del pueblo, tales como ropa, material didáctico, juguetes, etc.
               </p>
               <p>
-                Phasellus faucibus ante non libero scelerisque, id posuere odio finibus. Maecenas tristique a turpis in tristique. Phasellus augue dolor, porttitor in cursus vel, consectetur at est. Maecenas venenatis mi sit amet fermentum mollis. Vivamus non purus sed velit facilisis lobortis. Phasellus et facilisis sem. Quisque tristique volutpat dictum. Nulla egestas magna et diam scelerisque fermentum.
+                –  Programar acciones solidarias para conseguir mejoras en el ámbito de la salud de sus habitantes y la construcción de infraestructuras en el pueblo.
               </p>
               <p>
-                Etiam tincidunt cursus tincidunt. Mauris ut augue nisi. Aliquam porttitor justo sit amet velit volutpat viverra. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis non urna consequat, rutrum urna at, ultrices ante. Nam et efficitur erat. Curabitur in blandit mauris, sed dapibus nisl. Integer egestas ligula porta, egestas augue finibus, tincidunt ex. Duis consequat enim et lorem facilisis, rhoncus commodo diam mollis. Donec blandit felis commodo, rutrum ligula at, suscipit felis. Nunc nec orci vitae lorem mattis faucibus sed eu purus. Phasellus convallis aliquam risus vel sollicitudin. Maecenas at tempor tortor, at egestas sem.
+                –  Favorecer y promover intercambios culturales entre los voluntarios, coordinadores y la población, desde el respeto y el aprendizaje, para conocer la cultura y tradiciones, y desarrollar compromisos personales en actividades colaborativas.
               </p>
+              <p>
+                –  Elaborar proyectos culturales y sociales en colaboración con otros organismos y asociaciones para la ayuda y mejora de la población.
+              </p>
+              <p>
+                –  Elaborar proyectos educativos y actividades lucrativas-culturales para la mejora del desarrollo de los niños.
+              </p>
+              <p>
+                –  Desarrollar compromisos personales y sociales mediante la realización de actividades en grupo entre niños y adultos o entre adultos de diferentes procedencias con pensamientos muy dispares para lograr una superación personal por parte de todos los individuos.
+              </p>
+
             </Col>
           </Row>
         </div>
--- src/pages/LandingPage/components/Projects/Projects.js
+++ src/pages/LandingPage/components/Projects/Projects.js
@@ -2,21 +2,35 @@ import React from 'react'
 import { Container, Row } from "reactstrap"
 import { Project } from './Project'
 
-const Projects = () => {
-  return (
-    <div className="section section-team text-center">
-      <Container>
-        <h2 className="title">Proyectos</h2>
-        <div className="team">
-          <Row>
-            <Project projectName="Proyecto de primavera" />
-            <Project projectName="Proyecto de verano" />
-            <Project projectName="Proyecto de invierno" />
-          </Row>
+
+class Projects extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.props = props;
+
+    this.state = {
+    }
+
+  }
+
+  render() {
+    return (
+        <div className="section section-team text-center">
+          <Container>
+            <h2 className="title">Proyectos</h2>
+            <div className="team">
+              <Row>
+                <Project attr={{img:require('assets/img/stSanta.png'), text:'Del 12/04 al 22/04', projectName:'Proyecto de primavera'}} />
+                <Project attr={{img:require('assets/img/verano.jpg'), text:'Del 27/07 al 10/08', projectName:'Proyecto de verano'}} />
+                <Project attr={{img:require('assets/img/navidad_720.jpg'), text:'Del 25/12 al 06/01', projectName:'Proyecto de invierno'}} />
+              </Row>
+            </div>
+          </Container>
         </div>
-      </Container>
-    </div>
-  )
-}
+    )
+  }
+
+};
 
 export { Projects }
