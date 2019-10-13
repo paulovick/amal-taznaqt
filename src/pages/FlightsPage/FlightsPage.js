b0f2d1f4185cd8e6fa070555984c789cab83e906
Pau Torrents <pau.torrents@thoughtworks.com>
2019-10-13 03:36:52 +0200

    Added form to flights

--- src/pages/FlightsPage/FlightsPage.js
+++ src/pages/FlightsPage/FlightsPage.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react'
 import { fetchFlights } from "../../services/skyscanner"
 import { Flight } from "./Flight/Flight"
-import { Col, Container, Row } from "reactstrap"
+import { Col, Container, Form, FormGroup, Input, Row } from "reactstrap"
 
 import "./FlightsPage.scss"
 import { FlightsTable } from "./FlightsTable/FlightsTable"
+import Datetime from "react-datetime"
 
 class FlightsPage extends Component {
   state = {
+    origin: { id: 'BCN-sky', label: 'Barcelona - El Prat' },
     outboundDays: ["2019-11-10", "2019-11-11", "2019-11-12", "2019-11-13", "2019-11-14", "2019-11-15"],
     outboundFlights: [],
-    returnDays: ["2019-11-28", "2019-11-29", "2019-11-30", "2019-12-01", "2019-12-02", "2019-12-03"],
+    returnDays: ["2019-11-28", "2019-11-29", "2019-11-30", "2019-12-01"],
     returnFlights: []
   }
 
@@ -43,6 +45,48 @@ class FlightsPage extends Component {
             <h2 className="title">Vuelos</h2>
           </Col>
         </Row>
+
+        <Form className="form-flights">
+          <FormGroup>
+            <label htmlFor="originSelect" className="outbound-from-label">Salida des de</label>
+            <Input id="originSelect" type="select" className="origin-airport-select">
+              <option value="BCN-sky">Barcelona - El Prat</option>
+              <option value="MAD-sky">Madrid - Barajas</option>
+            </Input>
+          </FormGroup>
+          <div className="dates-container">
+            <FormGroup className="dates-period-container">
+              <label htmlFor="outboundStartDate">Selecciona un rango de fechas para la salida</label>
+              <div className="flights-page-dates">
+                <Datetime
+                  id="outboundStartDate"
+                  className="flights-page-datepicker"
+                  timeFormat={false}
+                />
+                <Datetime
+                  id="outboundEndDate"
+                  className="flights-page-datepicker"
+                  timeFormat={false}
+                />
+              </div>
+            </FormGroup>
+            <FormGroup className="dates-period-container">
+              <label htmlFor="returnStartDate">Selecciona un rango de fechas para la salida</label>
+              <div className="flights-page-dates">
+                <Datetime
+                  id="returnStartDate"
+                  className="flights-page-datepicker"
+                  timeFormat={false}
+                />
+                <Datetime
+                  id="returnEndDate"
+                  className="flights-page-datepicker"
+                  timeFormat={false}
+                />
+              </div>
+            </FormGroup>
+          </div>
+        </Form>
 
         <h3>Ida y vuelta más baratos</h3>
         {cheapestOutboundFlight && !cheapestReturnFlight &&
