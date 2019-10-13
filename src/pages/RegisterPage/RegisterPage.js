cce7018809030cc20a3f332ac93ef04bf11967e9
Marc Capdevila <cramca49@gmail.com>
2019-10-13 04:47:03 +0200

    register

--- src/pages/RegisterPage/RegisterPage.js
+++ src/pages/RegisterPage/RegisterPage.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import LandingPageHeader from "../LandingPage/components/LandingPageHeader/LandingPageHeader";
+import { ReactTypeformEmbed } from 'react-typeform-embed';
 
-const RegisterPage = () => {
-  return (
-    <div>Register Page</div>
-  )
-}
+import './RegisterPage.scss'
+
+class RegisterPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.props = props;
+
+    this.state = {}
+
+  }
+
+  componentDidMount() {}
+
+  render() {
+    return (
+        <div className={'register-page'}>
+          <LandingPageHeader/>
+          <div className={'register-page-card-container'}>
+            <ReactTypeformEmbed url="https://developerplatform.typeform.com/to/EYEzpA" />;
+          </div>
+        </div>
+    )
+  }
+
+
+
+};
 
 export { RegisterPage }
